541cbeeb3c4417f6f2191c1efe8942df630df724
Jussi Rajaniemi <jrajaniemi@mc-lifestyle.com>
2017-08-14 08:05:27 +0300

    Add theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+
+export class Speak {
+  text: string;
+  voice: string;
+  volume: number;
+  rate: number;
+  pitch: number;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +17,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  speak: Speak = {
+    text: '',
+    voice: 'Satu',
+    volume: 1.0,
+    rate: 0.1,
+    pitch: 1
+  };
+
+  voice: SpeechSynthesisVoice;
+  test = '';
+
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
+  constructor() {
+    this.loadVoices();
+  }
+
+  loadVoices() {
+    // Fetch the available voices.
+    const voices = speechSynthesis.getVoices();
+    const lang = 'fi-FI';
+    // Loop through each of the voices.
+    this.voice = voices.find(i => i.lang === lang);
+    console.log(this.voice);
+  }
+
+
+  speakToMe() {
+    this.loadVoices();
+    this.test = this.speak.text;
+    if ('speechSynthesis' in window) {
+      this.test = 'Selaimesi tukee puhesyntetisaattoria';
+    } else {
+      this.test = 'Pahoittelen ettei selaimesi tue puhesyntetisaattoria';
+    }
+
+    const msg = new SpeechSynthesisUtterance();
+    msg.volume = this.speak.volume;
+    msg.pitch = this.speak.pitch;
+    msg.rate = this.speak.rate;
+    msg.voice = this.voice;
+    if (this.speak.text.length > 0) {
+      msg.text = this.speak.text;
+      window.speechSynthesis.speak(msg);
+    } else {
+      msg.text = 'Kirjoita teksti, niin voin lausua sen';
+      window.speechSynthesis.speak(msg);
+    }
+  }
+
+  onKey(event: any) {
+    if (event.keyCode === 13) {
+      this.speakToMe();
+    }
+  }
+
+  clear() {
+    this.speak.text = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {  MdButtonModule,
+          MdToolbarModule,
+          MdCheckboxModule,
+          MdMenuModule,
+          MdIconModule,
+          MdInputModule,
+          MdCardModule,
+          MdSelectModule
+      } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import 'hammerjs';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    MdMenuModule,
+    MdButtonModule,
+    MdIconModule,
+    MdCheckboxModule,
+    MdToolbarModule,
+    MdInputModule,
+    FormsModule,
+    MdSelectModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
